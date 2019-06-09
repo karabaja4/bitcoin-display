923b59a3a1385a6bf12e1ae8b4b67a48818873d4
karabaja4 <karabaja4@gmail.com>
2019-06-09 03:01:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bitcoinlcdticker4/FullscreenActivity.java
@@ -0,0 +1,93 @@
+package com.example.bitcoinlcdticker4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+import android.graphics.Typeface;
+import android.os.Handler;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FullscreenActivity extends Activity
+{
+    private int mInterval = 5000;
+    private Handler mHandler;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+
+        TextView tx = findViewById(R.id.fullscreen_content);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/7segment.ttf");
+        tx.setTypeface(custom_font);
+
+        mHandler = new Handler();
+        mQueue = Volley.newRequestQueue(this);
+        startRepeatingTask();
+    }
+
+    Runnable mStatusChecker = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            try
+            {
+                updateStatus();
+            }
+            finally
+            {
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    private void updateStatus()
+    {
+        final TextView tx = findViewById(R.id.fullscreen_content);
+        String url ="https://api.coinbase.com/v2/prices/BTC-USD/spot";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>()
+        {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+                try
+                {
+                    JSONObject o = response.getJSONObject("data");
+                    tx.setText(String.valueOf((int)o.getDouble("amount")));
+                }
+                catch (JSONException e)
+                {
+                    tx.setText("N/A");
+                }
+            }
+        },
+        new Response.ErrorListener()
+        {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+                tx.setText("N/A");
+            }
+        });
+
+        mQueue.add(request);
+    }
+
+    void startRepeatingTask()
+    {
+        mStatusChecker.run();
+    }
+}
